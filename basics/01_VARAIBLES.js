4f58c5a6c4d20f832165da518ff9e4cf30748a66
hemanthh1999 <104848996+hemanthh1999@users.noreply.github.com>
2025-05-28 15:07:53 +0000

    varaibles

---
+++ basics/01_VARAIBLES.js
@@ -0,0 +1,13 @@
+const accountId = 14453
+let accountEmail ="hemu@google.com"
+var accountPassword="123456"
+accountCity = "Jaipur"
+
+// accountId =2 not allowed
+accountEmail="hc@hc.com"
+accountPassword="2122122"
+accountCity="delhi"
+
+
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity]);
